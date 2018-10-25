6882717f48b5b257d8efcb23a19422cc7e5d8381
山﨑 ひかり <h_yamasaki@yumemi.co.jp>
2018-10-26 08:28:01 +0900

    install webpack

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+model.exports = {
+    entry: __dirname + "/src/test.js",
+    output: {
+        path: __dirname +'/dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            //
+        ]
+    }
+};
