495805d153e2d70b4e9343f1a49b3024a66e1c70
山﨑 ひかり <h_yamasaki@yumemi.co.jp>
2018-10-26 08:39:28 +0900

    edit the name of config

---
+++ webpack.config.babel.js
@@ -0,0 +1,9 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: 'index.js',
+  output: {
+    path: 'dist',
+    filename: 'index_bundle.js'
+  },
+  plugins: [new HtmlWebpackPlugin()]
+}
