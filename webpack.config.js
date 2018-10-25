843f02389c4442e2c5b4e5ff31aa14ba6d6b4c90
山﨑 ひかり <h_yamasaki@yumemi.co.jp>
2018-10-26 08:34:27 +0900

    edit config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
 model.exports = {
-    entry: __dirname + "/src/test.js",
-    output: {
-        path: __dirname +'/dist',
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            //
-        ]
-    }
+	entry: __dirname + "/src/test.js",
+	output: {
+		path: __dirname +'/dist',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,//ビルド対象のファイルを指定
+				loader: 'babel-loader',//loaderを指定
+				exclude: /node_modules/,
+				query: {////loaderに渡したいクエリパラメータを指定
+					presets: ['es2015','stage-0']
+				}
+			}
+		]
+	}
 };
