ddf13509710a13ff34cd043d8d3ae415f8541898
山﨑 ひかり <h_yamasaki@yumemi.co.jp>
2018-11-11 00:34:02 +0900

    webpack-dev-serverをインストール

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mode: 'development',
+  entry: __dirname + '/src/app.js',
+  output: {
+    path: __dirname + 'dist',
+    filename: 'bundle.js',
+  },
+  model: {
+    rules:[]
+  }
+}
