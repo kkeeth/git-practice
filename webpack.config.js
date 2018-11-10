32810d1fb8ca61eba38b89f4483917aed67a6e1d
山﨑 ひかり <h_yamasaki@yumemi.co.jp>
2018-11-11 01:10:28 +0900

    rulesを追加

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,16 @@ module.exports = {
     filename: 'bundle.js',
   },
   module: {
-    rules:[]
+    rules:[
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ]
+      }
+    ]
   }
 }
